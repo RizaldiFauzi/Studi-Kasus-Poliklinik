804e77e36c267883539f0c316051fb9034a47777
RizaldiFauzi <101273905+RizaldiFauzi@users.noreply.github.com>
2023-10-27 15:09:31 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,1 @@
-<?php 
-$databaseHost = 'localhost';
-$databaseName = 'poliklinik';
-$databaseUsername = 'root';
-$databasePassword = '';
- 
-$mysqli = mysqli_connect($databaseHost, 
-    $databaseUsername, $databasePassword, $databaseName);
+
